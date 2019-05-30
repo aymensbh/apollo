ac509b2dd8efd0cf173ec55b2c8c434fef7f7a9d
aymensbh <49540075+aymensbh@users.noreply.github.com>
2019-05-30 21:02:40 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:apollo/Utils/firebasehelper.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'Routes/homePage.dart';
+import 'Routes/welcome.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    title: "Apollo",
+    theme: ThemeData(),
+    home: Router(),
+  ));
+}
+
+class Router extends StatefulWidget {
+  @override
+  _RouterState createState() => _RouterState();
+}
+
+class _RouterState extends State<Router> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<FirebaseUser>(
+      stream: firebaseAuth.onAuthStateChanged,
+      builder: (context, snapshot) {
+        return AnimatedSwitcher(
+      duration: const Duration(seconds: 2),
+      child: snapshot.hasData
+        ? initUser(snapshot.data)
+        : WelcomePage()
+    );
+      },
+    );
+  }
+
+  Widget initUser(FirebaseUser fbuser){
+    firebaseUser= fbuser;
+    user= Firestore.instance.collection("user").document(firebaseUser.uid);
+    return HomePage();
+  }
+}
